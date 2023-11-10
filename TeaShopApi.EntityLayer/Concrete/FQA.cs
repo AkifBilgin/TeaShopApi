3abfcec0cb3f647d0ada4bf8c0b9ca390bcef243
Akif Bilgin <akif.bil@gmail.com>
2023-11-10 20:15:56 +0100

    Add project files.

---
+++ TeaShopApi.EntityLayer/Concrete/FQA.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaShopApi.EntityLayer.Concrete
+{
+    public class FQA
+    {
+        public int FQAID { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+       
+    }
+}
