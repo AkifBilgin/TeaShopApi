9de369981218d78ea06a263bfad5c1b926d0ed95
Akif Bilgin <akif.bil@gmail.com>
2023-11-14 20:00:18 +0100

    DataAccessLayer, BusinessLayer

---
+++ TeaShopApi.DataAccessLayer/Migrations/20231114172419_migFirst.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeaShopApi.DataAccessLayer.Migrations
+{
+    public partial class migFirst : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Drinks",
+                columns: table => new
+                {
+                    DrinkID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DrinkName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DrinkPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    DrinkImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drinks", x => x.DrinkID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FQAs",
+                columns: table => new
+                {
+                    FQAID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FQAs", x => x.FQAID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Messages",
+                columns: table => new
+                {
+                    MessageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MessageSenderName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageSubject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageEmail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageDetail = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessageSendDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Messages", x => x.MessageID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Testimonials",
+                columns: table => new
+                {
+                    TestimonialID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TestimonialName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestimonialComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    TestimonialImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Testimonials", x => x.TestimonialID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Drinks");
+
+            migrationBuilder.DropTable(
+                name: "FQAs");
+
+            migrationBuilder.DropTable(
+                name: "Messages");
+
+            migrationBuilder.DropTable(
+                name: "Testimonials");
+        }
+    }
+}
